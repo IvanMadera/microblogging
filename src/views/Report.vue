13c3fd497be0b200ad1e314438d1bcda86897f60
Ivan <ivan.madera1999@gmail.com>
2021-07-03 12:45:04 -0500

    Crud is already

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -4,14 +4,30 @@
         <Cabecera title="Reportes"></Cabecera>
         <div class="container has-text-left">
             <b-table striped hoverable :data="data">
-                <b-table-column label="ID" v-slot="props">
+                <b-table-column field="data.id" label="ID" v-slot="props">
                     {{props.row.id}}
                 </b-table-column>
-                <b-table-column label="first name">
+                <b-table-column field="data.f_name" label="Nombre" v-slot="props">
+                    {{props.row.f_name}}
                 </b-table-column>
-                <b-table-column label="date" centered>
+                <b-table-column field="data.l_name" label="Apellido" v-slot="props">
+                    {{props.row.l_name}}
+                </b-table-column>
+                <b-table-column field="data.date" label="Fecha" v-slot="props" centered>
                     <span class="tag is-success">
+                        {{props.row.date}}
                     </span>
+                </b-table-column>
+                <b-table-column field="data.reports" label="Reportes" v-slot="props" centered>
+                    <span class="tag is-warning">
+                        {{props.row.reports}}
+                    </span>
+                </b-table-column>
+                <b-table-column field="data.content" label="Contenido" v-slot="props">
+                    {{props.row.content}}
+                </b-table-column>
+                <b-table-column>
+                    <b-button class="is-fullwidth" type="is-danger" icon-right="delete" size="is-small"/>
                 </b-table-column>
             </b-table>
         </div>
@@ -31,35 +47,11 @@ export default {
     data() {
         return {
             data: [
-                { id: '1', first_name: 'Jesse', last_name: 'Simmons', date: '2016-10-15 13:43:27', gender: 'Male' },
-                { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
-                { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
-                { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
-                { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
-            ],
-            columns: [
-                {
-                    field: 'id',
-                    label: 'ID',
-                    width: '40',
-                    numeric: true
-                },
-                {
-                    field: 'first_name',
-                    label: 'First Name',
-                },
-                {
-                    field: 'last_name',
-                    label: 'Last Name',
-                },
-                {
-                    field: 'date',
-                    label: 'Date',
-                },
-                {
-                    field: 'gender',
-                    label: 'Gender',
-                }
+                { id: '1', f_name: 'Ivan', l_name: 'Madera', date: '2016-10-15', reports: 10, content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, voluptatibus.'},
+                { id: '2', f_name: 'Carlos', l_name: 'Moo', date: '2018-08-13', reports: 15, content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio alias doloremque.'},
+                { id: '3', f_name: 'Chris', l_name: 'Ruz', date: '2020-01-22', reports: 24, content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, blanditiis.'},
+                { id: '4', f_name: 'Andres', l_name: 'Ortiz', date: '2019-12-06', reports: 2, content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, enim.'},
+                { id: '5', f_name: 'Fernando', l_name: 'Herrera', date: '2017-04-19', reports: 21, content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, ut.'}
             ]
         }
     }    
