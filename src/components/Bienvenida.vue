b56f92c9db1e428fad3bcccea99069036c6a9ad8
Ivan <ivan.madera1999@gmail.com>
2021-07-07 13:40:08 -0500

    Reward button

--- src/components/Bienvenida.vue
+++ src/components/Bienvenida.vue
@@ -2,7 +2,7 @@
     <div class="hero is-primary is-fullheight is-bold">
         <div class="hero-body">
             <div class="container has-text-centered">
-                <h1 class="is-size-1"><strong>{{title}}</strong></h1>
+                <h1 class="title is-size-1"><strong>{{title}}</strong></h1>
                 <p class=is-size-5>{{subtitle}}</p>
                 <div class="buttons mt-5 is-centered">
                     <b-button rounded inverted outlined icon-left="plus-thick" type="is-primary" tag="router-link" :to="{ path: '/' }">
