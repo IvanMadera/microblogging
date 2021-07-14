d0df22ef898765089bf63b7e52f6b90e448c3875
CarlosMoo <LE17081431@merida.tecnm.mx>
2021-07-13 19:44:51 -0500

    YA CHUCHA DEJAME BAJAR LOS CAMBIOS

--- src/components/Muro.vue
+++ src/components/Muro.vue
@@ -66,18 +66,46 @@
                     <b-button @click="showComment = !showComment" v-model="showComment" expanded class="is-info" label="Discuss" type="is-light" icon-left="comment-multiple-outline"/>
                     <b-button expanded class="is-link" label="Share" type="is-light" icon-left="share-all-outline"/>      
             </footer>
-        </div><br>
-        <div class="media m-2" v-show="showComment">
-            <figure class="media-left image is-48x48">
-                <img class="is-rounded" src="https://bulma.io/images/placeholders/64x64.png">
-            </figure>
-            <div class="media-content">
-                <b-field class="mt-1">
-                    <b-input  class="is-flex is-flex-wrap-wrap" expanded placeholder="Escribe un comentario" maxlength="140"></b-input>
-                    <p class="control">
-                        <b-button class="button is-success" icon-right="send"></b-button>
-                    </p>
-                </b-field>
+        </div>
+        <div>
+            <div class="media m-2" v-show="showComment">
+                <div class>
+                    <b-button
+                    v-model="showMostrar"
+                    type="is-text"
+                    v-show="showMostrar"
+                    @click="showMostrar = !showMostrar, showSubComment = !showSubComment">Mostrar los comentarios de la publicación</b-button>
+                </div>
+                <div>
+                    <figure class="media-left image is-48x48">
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/64x64.png">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <b-field class="mt-1">
+                        <b-input  class="is-flex is-flex-wrap-wrap" expanded placeholder="Escribe un comentario" maxlength="140"></b-input>
+                        <p class="control">
+                            <b-button class="button is-success" icon-right="send"></b-button>
+                        </p>
+                    </b-field>
+                </div>
+                    <article class="media"  v-show="showSubComment">
+                        <figure class="media-left">
+                            <p class="image is-48x48">
+                            <img src="https://bulma.io/images/placeholders/96x96.png">
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>Carlos Iván Moo Barrera</strong>
+                                    <br>
+                                    Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
+                                    <br>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
             </div>
         </div>
     </div>
@@ -97,6 +125,8 @@ export default {
         return {
             showComment: false,
             showPublication: true,
+            showSubComment: false,
+            showMostrar: true,
             nameGift: { icon: 'gift', text: 'Premios'},
             gifts:[
                 { icon: 'hexagon-outline', text: '20 Exp'},
